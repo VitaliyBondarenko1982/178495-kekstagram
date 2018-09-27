d28784e3be23154351c43010fbe59f4a4bb175d4
VitaliyBondarenko1982 <vitaliybondarenko@ukr.net>
2018-09-27 17:05:12 +0300

    adding backend.js

--- js/picture.js
+++ js/picture.js
@@ -1,21 +1,5 @@
 'use strict';
 
 (function () {
-  // Подставляет данные из массива объектов в фрагменты и встраивает их на страницу
 
-  var renderPhotoCards = function (arr) {
-    var photoTemplateNode = document.querySelector('#picture').content.querySelector('.picture');
-
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < arr.length; i++) {
-      var photoElement = photoTemplateNode.cloneNode(true);
-      photoElement.querySelector('.picture__img').src = arr[i].url;
-      photoElement.querySelector('.picture__comments').textContent = arr[i].comments.length;
-      photoElement.querySelector('.picture__likes').textContent = arr[i].likes;
-      fragment.appendChild(photoElement);
-    }
-    window.gallery.picturesList.appendChild(fragment);
-  };
-
-  renderPhotoCards(window.gallery.allPhotosArr);
 })();
